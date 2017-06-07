5db422903c7fe05f361f7d6b22ca8ee26af11d24
AlvyS <AlvyShih@hotmail.com>
2017-06-07 21:30:20 +0000

    removed console logs, commented code, and cleaned up indentation like a good boi

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,57 +2,44 @@ $(document).ready(function () {
 
 //---------------------------------- Create tweet element/object and prep for render ---------------------------------------//
 function createTweetElement(tweet) {
-  // console.log(tweet);
   let $avatar = $("<img/>", { src: tweet.user.avatars.small }).addClass("avatars");
   let $name = $('<span/>', { text: tweet.user.name }).addClass("fullName");
-  let $handle = $('<handle>').text(tweet.user.handle).addClass("handle"); //this format also works.
-
+  // same as above, but different syntax
+  let $handle = $('<handle>').text(tweet.user.handle).addClass("handle");
   let $flag = $('<i/>').addClass('fa fa-flag');
-  let $retweet = $('<i>').addClass('fa fa-retweet'); // <i> or <i/>
+  let $retweet = $('<i/>').addClass('fa fa-retweet');
   let $heart = $('<i/>').addClass('fa fa-heart');
-
-
   let $content = $('<p/>', { text: tweet.content.text }).addClass("content");
-
   let $age = $('<span/>', { text: tweet.created_at }).addClass("age");
   let $header = $('<header/>')
-
   let $footer = $('<footer/>')
-
   let $icons = $('<div/>').addClass("icons");
-
   let $article = $('<article/>').addClass('content-article');
-
   let $section = $('<section/>');
 
+  // Creation of new tweet
   $header.append($avatar, $name, $handle);
   $icons.append($flag, $retweet, $heart);
   $footer.append($age, $icons);
   $article.append($header, $content, $footer);
-  // $section.append($article);
     return $article;
-  // return $section;
-  // console.log($section);
-}
 
+}
 
 //----------------------------------------------- Render Tweets ----------------------------------------------------//
 function renderTweets(tweets) {
   for (let pastTweet of tweets) {
-    $('#old-tweets').prepend(createTweetElement(pastTweet));
+    $('#existing-tweets').prepend(createTweetElement(pastTweet));
   }
 }
-// renderTweets();
-loadTweets();
 
+loadTweets();
 
 //----------------------------------- Submit new tweet, POST method, Ajax ------------------------------------------//
 $('.new-tweet form').on('submit', function(event) {
   event.preventDefault();
-  // console.log('tweet button clicked, preventing default');
   let text = $('.new-tweet textarea');
   let tweetText = $(this).find('[name=text]').val();
-  // console.log("text is:", text);
 
   if (tweetText === '') {
     $('.too-short').slideDown(function() {
@@ -81,7 +68,7 @@ $('.new-tweet form').on('submit', function(event) {
 });
 
 
-//----------------------------- post new Tweets into old tweet thread, GET method, Ajax  ------------------------------//
+//----------------------------- post new Tweets into existing tweet thread, GET method, Ajax  ------------------------------//
 function loadTweets() {
   $.ajax({
     method: 'GET',
@@ -90,7 +77,6 @@ function loadTweets() {
     renderTweets(tweets);
   })
 };
-
 
 //-------------------------------------- Compose Button Event Handler --------------------------------------------------//
 $('.compose').on('click', function(event) {
@@ -104,55 +90,6 @@ $('.dual-compose').on('click', function(event) {
     $('#text').focus();
   });
 });
-
-//--------------------------------------- Template Database of Users(DUMMY DATA) --------------------------------------//
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 
 //------------------------------------------------------- Close -------------------------------------------------------------------//
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,15 +3,15 @@
 // });
 
 $(document).ready(function() {
-    $('.new-tweet textarea').on('keyup', function() {
-        let maxText = 140;
-        let textLength = $(this).val().length;
-        let textRemaining = maxText - textLength;
-        if (textRemaining < 0) {
-            $(this).parent().find('.counter').css('color', 'red')
-        } else {
-            $(this).parent().find('.counter').css('color', 'black')
-        }
-        $(this).parent().find('.counter').text(textRemaining);
-    });
+	$('.new-tweet textarea').on('keyup', function() {
+		let maxText = 140;
+		let textLength = $(this).val().length;
+		let textRemaining = maxText - textLength;
+		if (textRemaining < 0) {
+			$(this).parent().find('.counter').css('color', 'red')
+		} else {
+			$(this).parent().find('.counter').css('color', 'black')
+		}
+		$(this).parent().find('.counter').text(textRemaining);
+	});
 });
